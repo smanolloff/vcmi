657eb01fd1c743c02aa868c0ba37598d0ea47dac
Simeon Manolov <s.manolloff@gmail.com>
2023-11-15 00:34:40 +0200

    re-enable pytest binary

--- pyclient/main.cpp
+++ pyclient/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "pyclient.h"
+#include "pyclient/aitypes.h"
 
 int main(int argc, char * argv[]) {
     std::function<void(int)> wactioncb;
@@ -8,40 +9,13 @@ int main(int argc, char * argv[]) {
     std::string mapname(argc > 1 ? argv[1] : "simotest.vmap");
     int act(argc > 2 ? std::stoi(std::string(argv[2])) : 150);
 
-    int i = 0;
+    int i = act;
 
-    // Convert WPyCB -> PyCB
-    const MMAI::F_GetAction resultcb = [&i, &wactioncb, &act](const MMAI::Result &result) {
-        LOG("resultcb called");
-        // if (i < 5)
-        boost::thread t([&i, wactioncb, &act]() { wactioncb(act+i); });
-
-        i += 1;
+    MMAI::F_GetAction getaction = [&i](MMAI::Result r){
+        return MMAI::Action(i++);
     };
 
-    // Convert WPyCBInit -> PyCBInit
-    const MMAI::ActionCBCB actioncbcb = [&wactioncb](MMAI::F_Action actioncb) {
-        wactioncb = [actioncb](int act){ actioncb(act); };
-        LOG("actioncbcb called");
-    };
-
-
-    // Convert WPyCBInit -> PyCBInit
-    const MMAI::SysCBCB syscbcb = [](MMAI::F_Sys &syscb) {
-        LOG("syscbcb called");
-    };
-
-    // Convert WPyCBInit -> PyCBInit
-    const MMAI::F_InitReset resetcbcb = [](MMAI::F_Reset &resetcb) {
-        LOG("resetcbcb called");
-    };
-
-    // Convert WPyCBInit -> PyCBInit
-    const MMAI::F_InitRenderAnsi renderansicbcb = [](MMAI::F_RenderAnsi &renderansicb) {
-        LOG("renderansicbcb called");
-    };
-
-    auto cbprovider = MMAI::CBProvider{renderansicbcb, resetcbcb, syscbcb, actioncbcb, resultcb};
+    auto cbprovider = MMAI::CBProvider(getaction, "");
 
     // TODO: config values
     std::string resdir = "/Users/simo/Projects/vcmi-gym/vcmi_gym/envs/v0/vcmi/build/bin";
@@ -49,11 +23,11 @@ int main(int argc, char * argv[]) {
     // std::string mapname = "simotest-enchanters.vmap";
 
     LOG("Start VCMI");
-    preinit_vcmi(resdir, "debug");
+    init_vcmi(resdir, "trace", "trace", &cbprovider);
 
     // WTF for some reason linker says this is undefined symbol WTF
     // fix: check pyclient.cpp start_vcmi arguments!
-    start_vcmi(cbprovider, mapname);
+    start_vcmi(mapname);
 
     return 0;
 }
