093a12effe0301fc9282281485d3899ae806b49d
simo (windows) <s.manolloff@gmail.com>
2024-10-08 21:34:22 +0300

    WIP

--- CI/ios-mmai/before_install.sh
+++ CI/ios-mmai/before_install.sh
@@ -6,7 +6,7 @@ ARCHIVE_RELEASE=v0.0.0-test
 ARCHIVE_NAME=vcmi-libtorch-ios-arm64.txz
 
 ARCHIVE_URL="https://github.com/smanolloff/vcmi-libtorch-builds/releases/download/$ARCHIVE_RELEASE/$ARCHIVE_NAME"
-curl -fL "$ARCHIVE_URL" | tar -xJ
+curl -fL "$ARCHIVE_URL" | tar -xJ -C AI/MMAI
 
 [ -d libtorch ] || { ls -lah; echo "failed extract libtorch"; exit 1; }
 
