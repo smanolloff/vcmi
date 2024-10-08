51dbdb65382bc4219a29b6b2e6a0f21f98239a2b
Simeon Manolov <s.manolloff@gmail.com>
2024-10-08 18:41:20 +0300

    [BUILD-WINDOWS]

--- CI/ios-mmai/before_install.sh
+++ CI/ios-mmai/before_install.sh
@@ -2,12 +2,12 @@
 
 set -eux
 
-. CI/ios/before_install.sh
-
 ARCHIVE_RELEASE=v0.0.0-test
 ARCHIVE_NAME=vcmi-libtorch-ios-arm64.txz
 
 ARCHIVE_URL="https://github.com/smanolloff/vcmi-libtorch-builds/releases/download/$ARCHIVE_RELEASE/$ARCHIVE_NAME"
-curl -fL "$ARCHIVE_URL" | tar -xz -
+curl -fL "$ARCHIVE_URL" | tar -xJ
 
 [ -d libtorch ] || { ls -lah; echo "failed extract libtorch"; exit 1; }
+
+. CI/ios/before_install.sh
--- CI/linux-mmai/before_install.sh
+++ CI/linux-mmai/before_install.sh
@@ -2,12 +2,12 @@
 
 set -eux
 
-. CI/linux/before_install.sh
-
 ARCHIVE_RELEASE=v0.0.0-test
 ARCHIVE_NAME=vcmi-libtorch-linux-x64.txz
 
 ARCHIVE_URL="https://github.com/smanolloff/vcmi-libtorch-builds/releases/download/$ARCHIVE_RELEASE/$ARCHIVE_NAME"
-curl -fL "$ARCHIVE_URL" | tar -xz -
+curl -fL "$ARCHIVE_URL" | tar -xJ
 
 [ -d libtorch ] || { ls -lah; echo "failed extract libtorch"; exit 1; }
+
+. CI/linux/before_install.sh
