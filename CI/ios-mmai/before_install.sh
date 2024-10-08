8c46278501cbd5e30c513370e950ec4df4528025
Simeon Manolov <s.manolloff@gmail.com>
2024-10-08 18:37:06 +0300

    [BUILD-WINDOWS]

--- CI/ios-mmai/before_install.sh
+++ CI/ios-mmai/before_install.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -eux
 
-. CI/linux/before_install.sh
+. CI/ios/before_install.sh
 
 ARCHIVE_RELEASE=v0.0.0-test
-ARCHIVE_NAME=vcmi-libtorch-linux-x64.txz
+ARCHIVE_NAME=vcmi-libtorch-ios-arm64.txz
 
 ARCHIVE_URL="https://github.com/smanolloff/vcmi-libtorch-builds/releases/download/$ARCHIVE_RELEASE/$ARCHIVE_NAME"
 curl -fL "$ARCHIVE_URL" | tar -xz -
