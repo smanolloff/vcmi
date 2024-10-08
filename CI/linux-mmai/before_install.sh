028ec07753bd2492f26fdfe3902b3996e927f098
Simeon Manolov <s.manolloff@gmail.com>
2024-10-08 17:37:55 +0300

    [BUILD-WINDOWS]

--- CI/linux-mmai/before_install.sh
+++ CI/linux-mmai/before_install.sh
@@ -5,7 +5,7 @@ set -eux
 . CI/linux/before_install.sh
 
 FILE_NAME=vcmi-libtorch-v2.4.1-ios-arm64-v0.0.0-test.txz
-DOWNLOAD_URL="https://github.com/smanolloff/vcmi-libtorch-builds/releases/download/v1.0/$FILENAME"
+DOWNLOAD_URL="https://github.com/smanolloff/vcmi-libtorch-builds/releases/download/v1.0/$FILE_NAME"
 curl -fLo libtorch.txz.zip "$DOWNLOAD_URL"
 unzip -p libtorch.txz.zip | tar -xz -
 
