62bd1a54d77e105f6c0efdb59d4547d355e0a207
Simeon Manolov <s.manolloff@gmail.com>
2024-10-16 20:53:55 +0300

    fix git workflow

--- CI/fetch_libtorch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-release=$1
-platform=$2
-ARCHIVE_URL="https://github.com/smanolloff/vcmi-libtorch-builds/releases/download/$release/vcmi-libtorch-$platform.txz"
-
-curl -fL "$ARCHIVE_URL" | tar -xJ -C AI/MMAI
-
-[ -d AI/MMAI/libtorch ] || { ls -lah AI/MMAI; echo "failed extract libtorch"; exit 1; }
