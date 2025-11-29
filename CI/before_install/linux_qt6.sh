6d2a1feead1ee4a0e1ef9c5db28922cfd3cddea5
Simeon Manolov <s.manolloff@gmail.com>
2025-11-29 12:11:02 +0200

    fix linux ci

--- CI/before_install/linux_qt6.sh
+++ CI/before_install/linux_qt6.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 # https://github.com/microsoft/onnxruntime/discussions/6489
 ONNXRUNTIME_URL=https://github.com/microsoft/onnxruntime/releases/download/v1.18.1/onnxruntime-linux-x64-1.18.1.tgz
 ONNXRUNTIME_ROOT=/opt/onnxruntime
+sudo mkdir -p "$ONNXRUNTIME_ROOT"
 curl -fsSL "$ONNXRUNTIME_URL" | sudo tar -xzv --strip-components=1 -C "$ONNXRUNTIME_ROOT"
 sudo ldconfig "$ONNXRUNTIME_ROOT"
 
