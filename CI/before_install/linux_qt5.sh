80543d27dc046800c17fe35bf4d7bf910d2a9b9d
Simeon Manolov <s.manolloff@gmail.com>
2025-11-29 13:09:14 +0200

    fix linux

--- CI/before_install/linux_qt5.sh
+++ CI/before_install/linux_qt5.sh
@@ -8,7 +8,6 @@ ONNXRUNTIME_URL=https://github.com/microsoft/onnxruntime/releases/download/v1.18
 ONNXRUNTIME_ROOT=/opt/onnxruntime
 sudo mkdir -p "$ONNXRUNTIME_ROOT"
 curl -fsSL "$ONNXRUNTIME_URL" | sudo tar -xzv --strip-components=1 -C "$ONNXRUNTIME_ROOT"
-sudo ldconfig "$ONNXRUNTIME_ROOT"
 find "$ONNXRUNTIME_ROOT"  # print file tree
 
 APT_CACHE="${APT_CACHE:-${RUNNER_TEMP:-/tmp}/apt-cache}"
--- CI/before_install/linux_qt6.sh
+++ CI/before_install/linux_qt6.sh
@@ -8,8 +8,7 @@ ONNXRUNTIME_URL=https://github.com/microsoft/onnxruntime/releases/download/v1.18
 ONNXRUNTIME_ROOT=/opt/onnxruntime
 sudo mkdir -p "$ONNXRUNTIME_ROOT"
 curl -fsSL "$ONNXRUNTIME_URL" | sudo tar -xzv --strip-components=1 -C "$ONNXRUNTIME_ROOT"
-sudo ldconfig "$ONNXRUNTIME_ROOT"
-find "$ONNXRUNTIME_ROOT"  # print file tree
+find "$ONNXRUNTIME_ROOT"  # print directory tree
 
 APT_CACHE="${APT_CACHE:-${RUNNER_TEMP:-/tmp}/apt-cache}"
 sudo mkdir -p "$APT_CACHE"
