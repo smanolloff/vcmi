556207c4550038879069878c050529180e68fcb9
Simeon Manolov <s.manolloff@gmail.com>
2025-12-10 06:37:16 +0200

    MMAI: an ML-powered combat AI

--- CI/install_conan_dependencies.sh
+++ CI/install_conan_dependencies.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-RELEASE_TAG="2025-11-06"
+set -x
+
+RELEASE_TAG="2025-11-29"
 FILENAME="$1.tgz"
-DOWNLOAD_URL="https://github.com/vcmi/vcmi-dependencies/releases/download/$RELEASE_TAG/$FILENAME"
+DOWNLOAD_URL="https://github.com/smanolloff/vcmi-dependencies/releases/download/$RELEASE_TAG/$FILENAME"
 
 downloadedFile="$RUNNER_TEMP/$FILENAME"
 curl -Lo "$downloadedFile" "$DOWNLOAD_URL"
