4b314d6b964316c4b9f61965970d82264760a4d7
Simeon Manolov <s.manolloff@gmail.com>
2026-06-19 15:24:39 +0300

    add random-armies flag

--- ML/MLClient.h
+++ ML/MLClient.h
@@ -59,6 +59,7 @@ namespace ML {
         const int randomObstacles;
         const int townChance;
         const int warmachineChance;
+        const bool randomArmies;
         const int randomArmyValueMin;
         const int randomArmyValueMax;
         const int randomArmyTargetVar;
